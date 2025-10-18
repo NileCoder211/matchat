930945b73e1292b97d0c0a6e62b8af9fd7b3ee2a
Nilecoder211 <nilecoder8@gmail.com>
2025-10-18 21:24:23 +0300

    siignup section code refactory

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -35,8 +35,9 @@ export const signup = async (req, res)=>{
                 password: hashedPassword
             })
             if(newUser){
-                generateToken(newUser._id, res);
+                
                 await newUser.save();
+                generateToken(newUser._id, res);
                 res.status(201).json({
                     _id: newUser._id,
                     username: newUser.username,
