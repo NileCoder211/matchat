d6e5006aaa330dc728777e6bc01a43c0d688c514
NileCoder211 <nilecoder8@gmail.com>
2025-11-07 16:30:27 +0300

    User.js chnages to user.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
