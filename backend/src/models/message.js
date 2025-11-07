6fb825398bd0c1570259ee4401f038f7b7ac5791
NileCoder211 <nilecoder8@gmail.com>
2025-11-07 16:33:03 +0300

    Update message.js

--- backend/src/models/message.js
+++ backend/src/models/message.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import User from "./User.js";
+import User from "./user.js";
 
 const messageSchema = new mongoose.Schema({
     senderId:{type:mongoose.Schema.Types.ObjectId, ref:User, required:true},
