6fefa3b33a5b714744d6aac001f70b08d422bfe2
NileCoder211 <nilecoder8@gmail.com>
2025-11-07 18:12:29 +0300

    Change production base URL to relative path

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosIntance = axios.create({
-    baseURL: import.meta.env.NODE === "development" ? "http://localhost:3003/api" : "https://matchat-jepjc.sevalla.app/api",
+    baseURL: import.meta.env.NODE === "development" ? "http://localhost:3003/api" : "/api",
     withCredentials: true
 })
