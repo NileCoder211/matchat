a69b716188acc3992cd4e1c1fc24216a6c41c829
Nilecoder211 <nilecoder8@gmail.com>
2025-11-12 15:37:19 +0300

    node modules built for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,12 +25,12 @@ app.use("/api/messages", messageRoutes);
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
     
-    app.get( (req, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
     })
 }
 
-
+// "matchat": "file:..",
 server.listen(PORT, ()=>{
     console.log("Server is running on port " + PORT);
     connectionDB();
