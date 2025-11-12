f5dfe1f904de8288d97aa0d0e2c1cfd949961f5a
NileCoder211 <nilecoder8@gmail.com>
2025-11-12 18:11:03 +0300

    Remove redundant export of User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -12,5 +12,3 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema)
 export default User;
-
-export default mongoose.models.User || mongoose.model("User", userSchema);
